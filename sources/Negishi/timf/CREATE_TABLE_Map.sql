5e395e5e72a7b6fc7a0d88b9294a8be43af8bf78
colomeru <myongin422@gmail.com>
2017-02-04 00:11:43 +0900

    Mapテーブル作成sql スタートIDランダム指定 １～４９までpositionでーた入れ込み

---
+++ sources/Negishi/timf/CREATE_TABLE_Map.sql
@@ -0,0 +1,8 @@
+USE Fishing
+
+CREATE TABLE Map (
+    position    int     ,
+    fish        int     NOT NULL
+    id          int     ,
+    PRIMARY KEY(position)
+);
---
+++ sources/Negishi/timf/test.php
@@ -0,0 +1,65 @@
+<html>
+	<head>
+	  <title>Hello WebAPI</title>
+	  <style type="text/css">
+	  	#result{
+	  		border: 1px solid gray;
+	  		 width: 100px;
+	  		height: 80px;
+	  	}
+	  </style>
+	</head>
+	<body>
+		<div id="result"></div>
+		<form>
+			<Input id="testbtn" type="button" value="TEST" onclick="">
+		</form>
+
+		<form>
+			<Input id="update" type="button" value="UPDATE" onclick="">
+		</form>
+		
+		<?php 
+			$val = include("SetData.php");
+			if ($val) {
+				echo "OK";
+			} else	{
+				echo "NO";
+			};
+		?>
+
+		<script src="js/jquery-3.1.1.min.js"></script>
+		<script>
+		$(document).ready(function(){
+
+			/*
+			$.ajax({
+				  url: "/timf/getStartUserID.php"
+				, dataType: "json"
+				, success: function( json ) {
+						result = json;
+					  $("#result").html(result);
+				}
+			});
+			*/
+
+			$("#testbtn").click(function(){
+				$.ajax({
+					  url: "/timf/GetStartUserID.php"
+					, dataType: "json"
+					, success: function( json ) {
+							result = json;
+						  $("#result").html(result);
+					}
+				});
+		  	});
+		  	
+		  	$("#update").click(function(){
+		  		
+		  		window.location.reload();
+		  	});
+
+		});
+		</script>
+	</body>
+</html>
